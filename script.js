1eabe3eaac038aab963ebad5b17f75f4897084c4
angkasa-sa <myangkasa.sa@gmail.com>
2023-11-15 20:06:00 +0700

    development

---
+++ script.js
@@ -0,0 +1,17 @@
+// JS for TradingView chart
+ new TradingView.widget(
+  {
+    "autosize": true,
+    "symbol": "NASDAQ:AAPL",
+    "timezone": "Etc/UTC",
+    "theme": "light",
+    "style": "1",
+    "locale": "en",
+    "toolbar_bg": "green",
+    "enable_publishing": false,
+    "hide_legend": true,
+    "range": "12M",
+    "save_image": false,
+  "container_id": "tradingview"
+  }
+);
